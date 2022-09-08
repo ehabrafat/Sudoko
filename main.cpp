1fc9208b3664a35859050640583a88d0648bd1a9
Ehab Rafat <73589240+ehabrafat@users.noreply.github.com>
2022-09-08 14:29:57 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int rows[9][256];
+int cols[9][256];
+int blocks[3][3][256];
+
+
+bool solve(vector<vector<char>>& board, int i, int j) {
+	if (i == 9) return true;
+	if (j == 9) return solve(board, i + 1, 0);
+	if (board[i][j] != '.') return solve(board, i, j + 1);
+	for (char d = '1'; d <= '9'; ++d) {
+		if (rows[i][d] || cols[j][d] || blocks[i / 3][j / 3][d]) continue;
+		rows[i][d] = cols[j][d] = blocks[i / 3][j / 3][d] = true;
+		board[i][j] = d;
+		if(solve(board, i, j + 1)) return true;
+		board[i][j] = '.';
+		rows[i][d] = cols[j][d] = blocks[i / 3][j / 3][d] = false;
+	}
+	return false;
+}
+
+void solveSudoku(vector<vector<char>>& board) {
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			if (board[i][j] != '.') {
+				char d = board[i][j];
+				rows[i][d] = true;
+				cols[j][d] = true;
+				blocks[i / 3][j / 3][d] = true;
+			}
+		}
+	}
+	solve(board, 0, 0);
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cout << board[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
+}
+
+int main() {
+	vector<vector<char>> board(9, vector<char>(9));
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cin >> board[i][j];
+		}
+	}
+	solveSudoku(board);
+}
